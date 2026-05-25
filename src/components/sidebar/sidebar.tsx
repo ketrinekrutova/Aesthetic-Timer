922212ea0c37945ed886734ee8acb57ec983f6a1
ketrinekrutova <kemvk@mail.ru>
2026-05-25 14:04:45 +0300

    Добавить затемнение при открытии сайдбара

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,20 +1,26 @@
 import styles from './sidebar.module.css';
-import { X } from 'lucide-react';
+import {X} from 'lucide-react';
 
 
-function Sidebar({ isOpen, onClose}: { isOpen: boolean, onClose: () => void }) {
+function Sidebar({isOpen, onClose}: { isOpen: boolean, onClose: () => void }) {
 
     return (
-        <div className={`${styles.sidebar} ${isOpen ? styles.sidebarOpen : ''}`}>
-        <div className={styles.titleAndClose}>
-        <h1>Theme</h1>
-        <button className={styles.btn} onClick={onClose}><X size={20} /></button>
-        </div>
-            <h2>Backgrounds</h2>
-            <p>Тут будут фоны.</p>
-            <h2>Clock fonts</h2>
-            <p>Тут будут шрифты для часов.</p>
-        </div>);
+        <>
+            <div className={`${styles.drawer} ${isOpen ? styles.drawerOpen : ''}`} onClick={onClose}></div>
+            <div className={`${styles.sidebar} ${isOpen ? styles.sidebarOpen : ''}`}>
+
+                <div className={styles.titleAndClose}>
+                    <h2>Theme</h2>
+                    <button className={styles.btn} onClick={onClose} aria-label="Close"><X size={20}/></button>
+                </div>
+                <h3>Backgrounds</h3>
+                <p>Тут будут фоны.</p>
+                <h3>Clock fonts</h3>
+                <p>Тут будут шрифты для часов.</p>
+
+            </div>
+        </>);
+
 }
 
 export default Sidebar;
