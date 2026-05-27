5c2e14c57fc6c1c9d230ba887d9fd2b76a2d0770
ketrinekrutova <kemvk@mail.ru>
2026-05-27 19:45:44 +0300

    chore: remove unused files

---
+++ src/data/theme.ts
@@ -0,0 +1,15 @@
+const backgrounds = [
+    { id: 'green', name: 'Green Aura', src: '/backgrounds/green.jpg' },
+    { id: 'pink', name: 'Pink Aura', src: '/backgrounds/pink.jpg' },
+    { id: 'leopard', name: 'Leopard Print', src: '/backgrounds/leopard.jpg' },
+    { id: 'polka-dot', name: 'Polka dots', src: '/backgrounds/polka-dot.jpg' }
+];
+
+const fonts = [
+    { id: 'pixel', name: 'Pixel', fontFamily: '"Press Start 2P"' },
+    { id: 'default', name: 'Strong', fontFamily: '"Verdana"' },
+    { id: 'cute', name: 'Cute', fontFamily: '"Bagel Fat One"' },
+    { id: 'soft', name: 'Soft', fontFamily: '"Mali"' },
+];
+
+export const theme = {}
